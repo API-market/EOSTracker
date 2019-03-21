3436ebfc1481449f5c1d6f7146c816eb958b5480
Tray Lewin <tray.lewin@live.com>
2019-03-20 18:28:01 -0700

    point prod to ORE history node

--- src/environments/environment.prod.chain.ts
+++ src/environments/environment.prod.chain.ts
@@ -4,6 +4,6 @@ export const environment = {
   votingUrl: 'https://eosportal.io',
   appName: 'ORE Explorer',
   logoUrl: '/assets/logo.png',
-  blockchainUrl: 'https://ore.openrights.exchange',
+  blockchainUrl: 'https://history-p1.openrights.exchange',
   chainId: '7900eaca71d5b213d3e1e15d54d98ad235a7a5b8166361be78e672edeeb2b47a'
 };
